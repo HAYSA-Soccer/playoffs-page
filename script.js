f76b399aa92b9e50f60e78997fd5c8c4591687e7
dbm1982 <dbm1982@Hotmail.com>
2026-06-10 12:49:31 -0400

    Fetch teams data from Google Sheets instead of local JSON

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+const SHEET_URL = "https://script.google.com/macros/s/AKfycbwaqSktudVfYj2RrdZNnO-NP0LXbVspLc1MND_DnpTs26A7xmsfaLOuyViBbYs3YFnC/exec";
+  
 async function loadTeams() {
-  const res = await fetch('data/teams.json');
+  const res = await fetch(SHEET_URL);
   const teams = await res.json();
   const grid = document.getElementById('card-grid');
 
   const template = await fetch('template.html').then(r => r.text());
 
   teams
-    .filter(t => t.active) // only show active teams
+    .filter(t => String(t.active).toUpperCase() === "YES")
     .forEach(team => {
       let html = template
         .replace(/{{division}}/g, team.division)
@@ -19,9 +21,9 @@ async function loadTeams() {
         .replace(/{{opponent}}/g, team.opponent)
         .replace(/{{time}}/g, team.time)
         .replace(/{{field}}/g, team.field)
-        .replace(/{{path_next}}/g, team.path_next || 'TBD')
-        .replace(/{{path_then}}/g, team.path_then || 'TBD')
-        .replace(/{{path_final}}/g, team.path_final || 'TBD');
+        .replace(/{{path_next}}/g, team.path_next)
+        .replace(/{{path_then}}/g, team.path_then)
+        .replace(/{{path_final}}/g, team.path_final);
 
       const wrapper = document.createElement('div');
       wrapper.innerHTML = html;
